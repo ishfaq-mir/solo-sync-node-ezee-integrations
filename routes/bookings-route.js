7a479d2614e65477692c778d1ef7e07a9bdf2eae
ishfaqmir <ishfaqmir@almuqeet.systems>
2024-05-29 17:55:29 +0530

    little tweaks needed

--- routes/bookings-route.js
+++ routes/bookings-route.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const axios = require("axios");
 const FormData = require("form-data");
 
-router.post("/", async function (req, res, next) {
+router.post("/legal", async function (req, res, next) {
   console.log("here booking data", req.body);
   const formData = new FormData();
   formData.append(
@@ -94,72 +94,72 @@ router.post("/", async function (req, res, next) {
   });
 });
 
-module.exports = router;
+// module.exports = router;
 
-// router.post("/", async function (req, res, next) {
-//   let roomDetailsObject = {};
-//   req.body.rooms.forEach((room, index) => {
-//     roomDetailsObject[`Room_${index + 1}`] = { ...room };
-//   });
-//   const { check_in_date, check_out_date, Booking_Payment_Mode, Email_Address } =
-//     req.body;
+router.post("/", async function (req, res, next) {
+  let roomDetailsObject = {};
+  req.body.rooms.forEach((room, index) => {
+    roomDetailsObject[`Room_${index + 1}`] = { ...room };
+  });
+  const { check_in_date, check_out_date, Booking_Payment_Mode, Email_Address } =
+    req.body;
 
-//   console.log(
-//     check_in_date,
-//     check_out_date,
-//     Booking_Payment_Mode,
-//     Email_Address
-//   );
+  console.log(
+    check_in_date,
+    check_out_date,
+    Booking_Payment_Mode,
+    Email_Address
+  );
 
-//   const formData = new FormData();
-//   formData.append(
-//     "BookingData",
-//     JSON.stringify({
-//       Room_Details: {
-//         ...roomDetailsObject,
-//       },
-//       check_in_date,
-//       check_out_date,
-//       Booking_Payment_Mode,
-//       Email_Address,
-//       Source_Id: "",
-//       MobileNo: "7006202746",
-//       Address: "Hellowrd",
-//       State: "Jammu and Kashmir",
-//       Country: "India",
-//       City: "srinagar",
-//       Zipcode: "191113",
-//       Fax: "",
-//       Device: "",
-//       Languagekey: "",
-//       paymenttypeunkid: "",
-//     })
-//   );
-//   formData.append("HotelCode", `${process.env.EZEE_HOTEL_CODE}`);
-//   formData.append("APIKey", `${process.env.EZEE_AUTH_CODE}`);
+  const formData = new FormData();
+  formData.append(
+    "BookingData",
+    JSON.stringify({
+      Room_Details: {
+        ...roomDetailsObject,
+      },
+      check_in_date,
+      check_out_date,
+      Booking_Payment_Mode,
+      Email_Address,
+      Source_Id: "",
+      MobileNo: "7006202746",
+      Address: "Hellowrd",
+      State: "Jammu and Kashmir",
+      Country: "India",
+      City: "srinagar",
+      Zipcode: "191113",
+      Fax: "",
+      Device: "",
+      Languagekey: "",
+      paymenttypeunkid: "",
+    })
+  );
+  formData.append("HotelCode", `${process.env.EZEE_HOTEL_CODE}`);
+  formData.append("APIKey", `${process.env.EZEE_AUTH_CODE}`);
 
-//   axios
-//     .post(
-//       "https://live.ipms247.com/booking/reservation_api/listing.php?request_type=InsertBooking",
-//       formData,
-//       {
-//         headers: {
-//           ...formData.getHeaders(),
-//           Cookie:
-//             "AWSALB=mGou11NzT6f1gFsHvuLa2oQe3ui07ozrRl7kyGpbmeMSYdRs4OieSQJgd0SuVLQgtEPKnW0/Wt4BalrQeFeLZQ3j2ByFdLfZRs/AIkdFvw2nEctSrkaV5syCbTan; AWSALBCORS=mGou11NzT6f1gFsHvuLa2oQe3ui07ozrRl7kyGpbmeMSYdRs4OieSQJgd0SuVLQgtEPKnW0/Wt4BalrQeFeLZQ3j2ByFdLfZRs/AIkdFvw2nEctSrkaV5syCbTan; PHPSESSID=i7lrcuh8qv1eehpdb0ma3vhin5; SSID=9n677qs5qbju151a6l4jnehu67",
-//         },
-//       }
-//     )
-//     .then((response) => {
-//       console.log(response.data);
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
+  axios
+    .post(
+      "https://live.ipms247.com/booking/reservation_api/listing.php?request_type=InsertBooking",
+      formData,
+      {
+        headers: {
+          ...formData.getHeaders(),
+          Cookie:
+            "AWSALB=mGou11NzT6f1gFsHvuLa2oQe3ui07ozrRl7kyGpbmeMSYdRs4OieSQJgd0SuVLQgtEPKnW0/Wt4BalrQeFeLZQ3j2ByFdLfZRs/AIkdFvw2nEctSrkaV5syCbTan; AWSALBCORS=mGou11NzT6f1gFsHvuLa2oQe3ui07ozrRl7kyGpbmeMSYdRs4OieSQJgd0SuVLQgtEPKnW0/Wt4BalrQeFeLZQ3j2ByFdLfZRs/AIkdFvw2nEctSrkaV5syCbTan; PHPSESSID=i7lrcuh8qv1eehpdb0ma3vhin5; SSID=9n677qs5qbju151a6l4jnehu67",
+        },
+      }
+    )
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
-//   res.json({
-//     status: "success",
-//   });
-// });
+  res.json({
+    status: "success",
+  });
+});
 
 module.exports = router;
