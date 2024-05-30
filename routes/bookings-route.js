5f79168c581b4d9438aa404a45f1d5647a6578e5
ishfaqmir <ishfaqmir@almuqeet.systems>
2024-05-30 17:32:52 +0530

    minor tests

--- routes/bookings-route.js
+++ routes/bookings-route.js
@@ -1,8 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const axios = require("axios");
-// const FormData = require("form-data");
-
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
